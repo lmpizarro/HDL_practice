b09f82ed0384586ca1777bcaf51bd9d74d614a99
lmpizarro <lmpizarro@gmail.com>
2021-05-23 21:45:04 -0300

    trapezoidal filter

--- python/nmigen/dsp/lp_iir.py
+++ python/nmigen/dsp/lp_iir.py
@@ -6,7 +6,48 @@ from nmigen.back import verilog
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import math
 
+
+class Trapezoidal(Elaboratable):
+    def __init__(self, width, delay=2):
+        self.taps = int(math.pow(2, delay))
+        self.width = width
+        self.delay = delay
+        self.x = Signal(signed(width))
+        self.y = Signal(signed(width), reset=0)
+
+        weights = [-1] * self.taps
+        weights.extend([0] * self.taps)
+        weights.extend([1] * self.taps)
+
+        self.params = {"weights":weights}
+
+
+    def elaborate(self, platform):
+        m = Module()
+
+        self.delay_line0 = Array([Signal(self.width, reset=0) for e in range(3 * self.taps)])
+        
+        for i,_ in enumerate(self.delay_line0):
+            if i > 0:
+                m.d.sync += [self.delay_line0[i-1].eq(self.delay_line0[i])]
+
+
+        weighted_sum = sum([el*weight for el,weight in zip(self.delay_line0, self.params["weights"])])
+        m.d.comb += self.y.eq(weighted_sum>>self.delay)
+
+        m.d.comb += [self.delay_line0[i].eq(self.x)]
+
+        return m 
+
+    def ports(self)->List[Signal]:
+            return [self.x, self.y]
+
+    def set_test_stimulus(self, sti):
+        self.stimulus = sti
+
+ 
 
 class DelayLine(Elaboratable):
     def __init__(self, width, delay=10):
@@ -26,16 +67,19 @@ class DelayLine(Elaboratable):
             if i > 0:
                 m.d.sync += [self.delay_line[i-1].eq(self.delay_line[i])]
 
-        m.d.sync += [self.delay_line[i].eq(self.x)] 
+        m.d.comb += [self.delay_line[i].eq(self.x)] 
 
         return m 
 
     def ports(self)->List[Signal]:
             return [self.x, self.y]
 
-       
+    def set_test_stimulus(self, sti):
+        self.stimulus = sti
 
-class LowPassSinglePole(Elaboratable):
+      
+
+class LowPassFilter(Elaboratable):
     def __init__(self, width):
         self.width = width
         
@@ -57,8 +101,8 @@ class LowPassSinglePole(Elaboratable):
         # 1 2 2, 2 1 2, 2 2 1
         m.d.comb += [
                      self.x1.eq(self.x>>2),
-                     self.y1.eq(self.ymem1>>2),
-                     self.y2.eq(self.ymem2>>1),
+                     self.y1.eq(self.ymem1>>1),
+                     self.y2.eq(self.ymem2>>2),
                      
                      self.y.eq(self.x1 + self.y1 + self.y2)
                      
@@ -78,16 +122,13 @@ class LowPassSinglePole(Elaboratable):
     def set_test_stimulus(self, sti):
         self.stimulus = sti
 
-
-import math
-
-if __name__ == "__main__":
-    lp = LowPassSinglePole(width=10)
+def test_lp():
+    lp = LowPassFilter(width=10)
     dl = DelayLine(width=10, delay=3)
 
     stimulus = [0 for i in range(10)]
     stimulus.extend([125*i for i in range(4)])
-    stimulus.extend([int(500*math.exp(-i/32)) for i in range(200)])
+    stimulus.extend([int(500*math.exp(-i/64)) for i in range(800)])
     stimulus.extend([0 for i in range(100)])
     lp.set_test_stimulus(stimulus)
     sim = Simulator(lp)
@@ -117,15 +158,24 @@ if __name__ == "__main__":
     
     print(verilog.convert(lp, strip_internal_attrs=True, ports=ports))
 
+
+
+def test_dl():
     dl = DelayLine(width=10, delay=4)
+
+    stimulus = [0 for _ in range(3)]
+    stimulus.extend([10 for _ in range(10)])
+    stimulus.extend([0 for _ in range(10)])
+
+    dl.set_test_stimulus(stimulus)
+
     sim = Simulator(dl)
     sim.add_clock(1e-6)
     def process():
         k = 10
-
-        for v in range(10):
+        for v in dl.stimulus:
             yield Tick()
-            yield dl.x.eq(k)
+            yield dl.x.eq(v)
             print((yield dl.y))
 
     sim.add_sync_process(process)
@@ -133,3 +183,32 @@ if __name__ == "__main__":
     with sim.write_vcd("test1.vcd", "test1.gtkw", traces=ports):
         sim.run()
 
+if __name__ == "__main__":
+    dl = Trapezoidal(width=12, delay=3)
+
+    stimulus = [0 for _ in range(0)]
+    stimulus.extend([100 for i,_ in enumerate(range(100))])
+    stimulus.extend([100 for _ in range(10)])
+
+    dl.set_test_stimulus(stimulus)
+
+    sim = Simulator(dl)
+    sim.add_clock(1e-6)
+    signal_o = []
+    def process():
+        k = 10
+        for v in dl.stimulus:
+            yield Tick()
+            yield dl.x.eq(v)
+            signal_o.append((yield dl.y))
+
+    sim.add_sync_process(process)
+    ports = dl.ports()
+    with sim.write_vcd("test1.vcd", "test1.gtkw", traces=ports):
+        sim.run()
+
+    print(signal_o)
+
+    plt.plot(dl.stimulus)
+    plt.plot(signal_o)
+    plt.show()
