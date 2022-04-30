38125b9d31b2d4b09c172271b62e7561587eb46a
lmpizarro <lmpizarro@gmail.com>
2022-04-30 17:59:32 -0300

    second order

---
+++ curso-pola/proyecto/matrix/src/models/second_order.py
@@ -0,0 +1,63 @@
+import math
+
+from scipy import signal
+import matplotlib.pyplot as plt
+
+class SecondOrder:
+    def __init__(self, wn=1, damping=.5) -> None:
+        self.wn = wn
+        self.damping = damping
+        self.sigma = damping * wn
+        self.wd = wn * math.sqrt(1 - damping**2)
+        tr = 1.8 / wn
+        tp = math.pi / self.wd
+        Mp = math.exp(-math.pi*damping)
+        ts = 4.6 / self.sigma
+
+        self.perf = {'tr': tr, 'tp': tp, 'Mp': Mp, 'ts': ts}
+        self.T = 0.0
+        
+
+    def z_transform(self, fs=1):
+        a = self.sigma
+        b = self.wd
+        self.T = 1 / fs
+
+        b1 = math.exp(-a*self.T) * math.sin(b*self.T)
+        a0 = math.exp(-2*a*self.T)
+        a1 = -2*math.exp(-a*self.T) * math.cos(b*self.T)
+        a2 = 1
+        gain = 1 / .8 # self.wn * self. wn / b 
+
+        return {'ZFT':{'b1': b1, 'a2': a2, 'a1': a1, 'a0': a0, 'gain': gain},
+                'STF': {'b0': self.wn * self.wn, 'a2': 1, 
+                        'a1': self.damping*2*self.wn, 'a0': self.wn},
+                'perf': self.perf}
+
+if __name__ == '__main__':
+    so = SecondOrder()
+    zft = so.z_transform()
+
+    tf = signal.TransferFunction([so.wn**2], [1, 2*so.damping*so.wn, so.wn**2])
+    t,y = signal.step2(tf)
+    
+    plt.plot(t, y)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    plt.title('Step response for 1. Order Lowpass')
+    plt.grid()
+    plt.show()
+
+    tf = signal.TransferFunction([zft['ZFT']['gain'] * zft['ZFT']['b1'], 0], 
+                                 [zft['ZFT']['a2'], zft['ZFT']['a1'], zft['ZFT']['a0']], 
+                                 dt=so.T)
+    import numpy as np
+
+    t,y = signal.dstep(tf, n=20)
+    plt.step(t, np.squeeze(y))
+    plt.grid()
+    plt.xlabel('n [samples]')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+    print(tf.to_ss())
